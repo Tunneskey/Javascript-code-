6fd50b9d8c09ffac8e74712c45124320d84a5ad7
Balogun Olatunde Ayoade <105291683+Tunneskey@users.noreply.github.com>
2022-06-25 13:08:49 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var firstname = 'Balogun';
+var middlename = 'Olatunde';
+var lastname = 'John';
+var height = '1.78m';
+var country = 'Nigeria';
+
+Name= firstname + ' '+ middlename + ' '+ lastname;
+
+document.write('Name:' + ' ' + Name +  '<br/>');
+document.write('Height:' + ' ' + height +  '<br/>');
+document.write('Country:' + ' ' + country +  '<br/>');
